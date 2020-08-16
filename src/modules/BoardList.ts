4eb1e7afdd230aef647bfe71a4b8448a43a20f4a
soo777 <ssm19944@gmail.com>
2020-08-16 17:28:23 +0900

    리덕스 샘플 모듈 추가

---
+++ src/modules/BoardList.ts
@@ -0,0 +1,16 @@
+type BoardListState = {
+  board: object,
+}
+
+const initialState: BoardListState = {
+  board: [
+    'aaaaa',
+    'bbbbb',
+  ],
+};
+
+function boardList(state:BoardListState = initialState) {
+  return 0;
+}
+
+export default boardList;
