b7c3bd1a6495fecc823c2d4d0ed9436f739a748e
soo777 <ssm19944@gmail.com>
2020-08-27 22:30:10 +0900

    redux devtools extension 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
@@ -10,7 +11,12 @@ import rootSaga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware),
+  ),
+);
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
