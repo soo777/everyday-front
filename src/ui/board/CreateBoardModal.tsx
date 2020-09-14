d6b28281bef93c837427a0f9f7699feba60a574c
soo777 <ssm19944@gmail.com>
2020-09-14 23:18:18 +0900

    createBoard modal dimmer 처리

--- src/ui/board/CreateBoardModal.tsx
+++ src/ui/board/CreateBoardModal.tsx
@@ -4,19 +4,21 @@ import { Button, Input } from 'semantic-ui-react';
 function CreateBoardModal() {
   return (
     <>
-      <div className="createModal">
-        <div className="header">
-          Create Board
-        </div>
-        <div className="content">
-          <Input
-            className="input"
-            placeHolder="이름을 입력하세요"
-          />
-        </div>
-        <div className="footer">
-          <Button>확인</Button>
-          <Button>취소</Button>
+      <div className="dimmer">
+        <div className="createModal">
+          <div className="header">
+            Create Board
+          </div>
+          <div className="content">
+            <Input
+              className="input"
+              placeHolder="이름을 입력하세요"
+            />
+          </div>
+          <div className="footer">
+            <Button>확인</Button>
+            <Button>취소</Button>
+          </div>
         </div>
       </div>
     </>
