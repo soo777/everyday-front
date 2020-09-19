f7f76fefaf6453bdc216f40a140f74a7c10aec5e
soo777 <ssm19944@gmail.com>
2020-09-19 13:19:20 +0900

    boardPage layout 작업

---
+++ src/pages/BoardPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function BoardPage() {
+  return (
+    <>
+      <div className="board">
+        <div className="nav">
+          sidebar
+        </div>
+        <div className="content">
+          content
+        </div>
+        <div>
+          sidebar
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BoardPage;
--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 import BoardListPage from './BoardListPage';
+import BoardPage from './BoardPage';
 
 function Routes() {
   return (
     <>
       <Switch>
-        <Route path="/" render={ (props) => <BoardListPage /> } />
+        <Route path="/boardList" render={ (props) => <BoardListPage /> } />
+        <Route path="/" render={ (props) => <BoardPage /> } />
       </Switch>
     </>
   );
