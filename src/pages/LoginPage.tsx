9a50a9817f66bdf6fe5f0bb0ee248cd30e547e2f
soo777 <ssm19944@gmail.com>
2020-10-09 14:50:48 +0900

    Login api 연동

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { Button, Header, Input } from 'semantic-ui-react';
+import {
+  Button, Form, Header, Input,
+} from 'semantic-ui-react';
+import axios from 'axios';
 
 function LoginPage() {
-  const [id, setId] = useState<string>('');
+  const [userId, setUserId] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
   const handleIdInput = (e:React.ChangeEvent<HTMLInputElement>) => {
     const idInput = e.target.value;
-    setId(idInput);
+    setUserId(idInput);
   };
 
   const handlePasswordInput = (e:React.ChangeEvent<HTMLInputElement>) => {
@@ -15,37 +18,52 @@ function LoginPage() {
     setPassword(passwordInput);
   };
 
-  const login = () => {
-    console.log(id, password);
+  const login = async () => {
+    const payload = {
+      username: userId,
+      password,
+    };
+
+    await axios.post('/authenticate', payload).then((data) => {
+      console.log(data);
+      if (data.status === 200) {
+        console.log('login success');
+        localStorage.setItem('everyday_token', data.data.object);
+      } else {
+        console.log('login fail');
+      }
+    });
   };
 
   return (
     <>
       <div className="login">
-        <div className="header">
-          <Header as="h3">Login</Header>
-        </div>
-        <div className="login_input">
-          <Input
-            value={ id }
-            onChange={ handleIdInput }
-            placeholder="id"
-          />
-        </div>
-        <div className="login_input">
-          <Input
-            value={ password }
-            onChange={ handlePasswordInput }
-            placeholder="password"
-          />
-        </div>
-        <div className="login_btn">
-          <Button
-            onClick={ login }
-          >
-            Login
-          </Button>
-        </div>
+        <Form>
+          <div className="header">
+            <Header as="h3">Login</Header>
+          </div>
+          <div className="login_input">
+            <Input
+              value={ userId }
+              onChange={ handleIdInput }
+              placeholder="id"
+            />
+          </div>
+          <div className="login_input">
+            <Input
+              value={ password }
+              onChange={ handlePasswordInput }
+              placeholder="password"
+            />
+          </div>
+          <div className="login_btn">
+            <Button
+              onClick={ login }
+            >
+              Login
+            </Button>
+          </div>
+        </Form>
       </div>
     </>
   );
