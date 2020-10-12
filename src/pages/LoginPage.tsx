062db57c0b59647631ff1bb27927eb1bcd303f89
soo777 <ssm19944@gmail.com>
2020-10-12 22:32:34 +0900

    회원가입 modal ui 개발

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-  Button, Form, Header, Input,
+  Button, Form, Header, Input, Modal, Image, Table,
 } from 'semantic-ui-react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Constant } from 'config';
@@ -11,6 +11,7 @@ const axios = axiosInstance.instance;
 function LoginPage(routesProps: RouteComponentProps) {
   const [userId, setUserId] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [modalOpen, setModal] = useState<boolean>(true);
 
   const handleIdInput = (e:React.ChangeEvent<HTMLInputElement>) => {
     const idInput = e.target.value;
@@ -20,6 +21,10 @@ function LoginPage(routesProps: RouteComponentProps) {
   const handlePasswordInput = (e:React.ChangeEvent<HTMLInputElement>) => {
     const passwordInput = e.target.value;
     setPassword(passwordInput);
+  };
+
+  const signIn = () => {
+    setModal(true);
   };
 
   const login = async () => {
@@ -69,8 +74,79 @@ function LoginPage(routesProps: RouteComponentProps) {
               Login
             </Button>
           </div>
+          <div className="sign_div">
+            <span
+              className="signIn"
+              onClick={ signIn }
+            >
+              sign in
+            </span>
+          </div>
         </Form>
       </div>
+
+      <Modal
+        onClose={ () => setModal(false) }
+        onOpen={ () => setModal(true) }
+        open={ modalOpen }
+        size="tiny"
+      >
+        <Modal.Header>Sign In</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Table celled>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>
+                    ID
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Input />
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>
+                    Password
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Input type="password" />
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>
+                    Password 확인
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Input  type="password" />
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>
+                    이름
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Input />
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            content="Sign In"
+            labelPosition="right"
+            icon="checkmark"
+            onClick={ () => setModal(false) }
+            positive
+          />
+          <Button
+            content="cancel"
+            color="black"
+            onClick={ () => setModal(false) }
+          />
+        </Modal.Actions>
+      </Modal>
     </>
   );
 }
