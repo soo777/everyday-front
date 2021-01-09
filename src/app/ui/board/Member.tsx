c7e9bcda3c53b24a4952a0319ee09ae5ad40f386
soo777 <ssm19944@gmail.com>
2021-01-09 11:41:26 +0900

    boardList css 수정

--- src/app/ui/board/Member.tsx
+++ src/app/ui/board/Member.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
 
 function Member() {
+  const addMember = () => {
+    console.log('add member');
+  };
+
   return (
     <>
       <div className="middle content">
         <div>
           <div className="list header">
-            <h3>Member List</h3>
+            <div className="float_left">
+              <h3>Member List</h3>
+            </div>
+            <span className="plus" onClick={ addMember }>
+              <Icon name="plus" />
+            </span>
           </div>
-          <div className="list">
-            <div className="pd5 font15">
-              moon
-            </div>
-            <div className="pd5 font15">
-              kim
-            </div>
-            <div className="pd5 font15">
-              park
-            </div>
-            <div className="pd5 font15">
-              son
-            </div>
+        </div>
+        <div className="list">
+          <div className="pd5 font15">
+            moon
+          </div>
+          <div className="pd5 font15">
+            kim
+          </div>
+          <div className="pd5 font15">
+            park
+          </div>
+          <div className="pd5 font15">
+            son
           </div>
         </div>
       </div>
